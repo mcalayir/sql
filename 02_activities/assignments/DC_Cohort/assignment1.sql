a2c9bf0b74521edd86a83bc0597f2a64590059fb
mcalayir <muhammetcalayir@hotmail.com>
2025-11-09 13:34:00 -0500

    modified assignment1.sql in the new branch

--- 02_activities/assignments/DC_Cohort/assignment1.sql
+++ 02_activities/assignments/DC_Cohort/assignment1.sql
@@ -73,16 +73,18 @@ FROM product;
 --JOIN
 /* 1. Write a query that INNER JOINs the vendor table to the vendor_booth_assignments table on the 
 vendor_id field they both have in common, and sorts the result by vendor_name, then market_date. */
-SELECT v.vendor_id,
-       v.vendor_name,
-       v.vendor_owner,
-       v.vendor_focus,
-       vba.market_date,
-       vba.booth_id
-FROM vendor v
-INNER JOIN vendor_booth_assignments vba
+SELECT
+  v.vendor_id,
+  v.vendor_name,
+  v.vendor_type,
+  (v.vendor_owner_first_name || ' ' || v.vendor_owner_last_name) AS vendor_owner,
+  vba.market_date,
+  vba.booth_number
+FROM vendor AS v
+INNER JOIN vendor_booth_assignments AS vba
   ON v.vendor_id = vba.vendor_id
 ORDER BY v.vendor_name, vba.market_date;
+
 
 
 
@@ -102,12 +104,13 @@ sticker to everyone who has ever spent more than $2000 at the market. Write a qu
 of customers for them to give stickers to, sorted by last name, then first name. 
 
 HINT: This query requires you to join two tables, use an aggregate function, and use the HAVING keyword. */
-SELECT c.customer_id,
-       c.customer_first_name,
-       c.customer_last_name,
-       SUM(cp.quantity * cp.cost_to_customer_per_qty) AS total_spent
-FROM customer c
-JOIN customer_purchases cp
+SELECT 
+  c.customer_id,
+  c.customer_first_name,
+  c.customer_last_name,
+  SUM(cp.quantity * cp.cost_to_customer_per_qty) AS total_spent
+FROM customer AS c
+JOIN customer_purchases AS cp
   ON c.customer_id = cp.customer_id
 GROUP BY c.customer_id
 HAVING total_spent > 2000
@@ -125,22 +128,37 @@ When inserting the new vendor, you need to appropriately align the columns to be
 -> To insert the new row use VALUES, specifying the value you want for each column:
 VALUES(col1,col2,col3,col4,col5) 
 */
+-- Create a temporary copy of the vendor table
+DROP TABLE IF EXISTS new_vendor;
+
 CREATE TEMP TABLE new_vendor AS
 SELECT * FROM vendor;
 
-INSERT INTO new_vendor (vendor_id, vendor_name, vendor_focus, vendor_owner, vendor_description)
-VALUES (10, 'Thomass Superfood Store', 'Fresh Focused', 'Thomas Rosenthal', 'New vendor specializing in superfoods.');
-
-
+-- Insert a new 10th vendor (match the actual column names)
+INSERT INTO new_vendor (
+  vendor_id, 
+  vendor_name, 
+  vendor_type, 
+  vendor_owner_first_name, 
+  vendor_owner_last_name
+)
+VALUES (
+  10, 
+  'Thomass Superfood Store', 
+  'Fresh Focused', 
+  'Thomas', 
+  'Rosenthal'
+);
 
 -- Date
 /*1. Get the customer_id, month, and year (in separate columns) of every purchase in the customer_purchases table.
 
 HINT: you might need to search for strfrtime modifers sqlite on the web to know what the modifers for month 
 and year are! */
-SELECT customer_id,
-       STRFTIME('%m', purchase_date) AS month,
-       STRFTIME('%Y', purchase_date) AS year
+SELECT 
+    customer_id,
+    STRFTIME('%m', market_date) AS month,
+    STRFTIME('%Y', market_date) AS year
 FROM customer_purchases;
 
 
@@ -149,9 +167,13 @@ Remember that money spent is quantity*cost_to_customer_per_qty.
 
 HINTS: you will need to AGGREGATE, GROUP BY, and filter...
 but remember, STRFTIME returns a STRING for your WHERE statement!! */
-SELECT customer_id,
-       SUM(quantity * cost_to_customer_per_qty) AS total_spent_april_2022
+SELECT 
+  customer_id,
+  SUM(quantity * cost_to_customer_per_qty) AS total_spent_april_2022
 FROM customer_purchases
-WHERE STRFTIME('%m', purchase_date) = '04'
-  AND STRFTIME('%Y', purchase_date) = '2022'
+WHERE STRFTIME('%m', market_date) = '04'
+  AND STRFTIME('%Y', market_date) = '2022'
 GROUP BY customer_id;
+
+
+
